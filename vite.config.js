62478a58765ee3f0d509fab81c2ee027791352f1
Tiago Sousa Andrade <ts0418764@gmail.com>
2026-08-14 18:24:37 -0300

    fix: configurar base path para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Prototipo-futuriza-v2.0-final-teste/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
